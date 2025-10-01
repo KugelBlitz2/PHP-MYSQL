73ed9079dbe45317eba5f422d280efb7ee30c8ae
KugelBlitz2 <galica.orik@gmail.com>
2025-10-01 20:28:21 +0200

    Scope and global variabels

--- moduli1/app.php
+++ moduli1/app.php
@@ -1,4 +1,5 @@
 <?php
+/*
 #phpinfo();
 
 $x = "Data";
@@ -45,3 +46,51 @@ $b = 10;
 $game = mymax($a, $b);
 
 echo "The max of $a and $b is $game";
+
+
+$u = 5;
+
+function local() {
+    $x = 7;
+    echo $x;
+}
+
+local();
+echo "\n, $u";
+
+$x = 5;
+$y = 6;
+function sum() {
+    global $x, $y;
+    $z = $x + $y;
+}
+
+sum();
+echo $y;
+*/
+
+$sport = ["Volleyball", "Handball"];
+echo $sport[0];
+echo "<br>";
+echo end($sport);
+echo "<br>";
+echo count($sport);
+echo "<br>";
+$food = array("Hamburger", "Drink", "Chesse");
+for ($i=0; $i < 3; $i++) { 
+    echo $food[$i], "\n";
+
+}
+
+array_push($food, "apple");
+echo "<br>";
+var_dump($food);
+echo "<br>";
+array_pop($food);
+var_dump($food);
+echo "<br>";
+array_unshift($food, "china food");
+var_dump($food);
+echo "<br>";
+array_shift($food);
+var_dump($food);
