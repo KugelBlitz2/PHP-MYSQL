edcd2945571f9a7251621f04eed0904f183c8e19
KugelBlitz2 <galica.orik@gmail.com>
2025-10-08 20:39:58 +0200

    Update app.php
    1

--- moduli2/app.php
+++ moduli2/app.php
@@ -1,5 +1,39 @@
 <?php
+/*
+$consoles = array(
+    array("Nintendo Switch", "Japan", 138),
+    array("Steam Deck", "USA", 11),
+    array("ROG Ally Claw", "England", 5),
+);
 
+echo $consoles[0][0] . " Origin: " . $consoles[0][1] . " Units Sold: " . $consoles[0][2];
+echo $consoles[1][0] . " Origin: " . $consoles[1][1] . " Units Sold: " . $consoles[1][2];
+echo $consoles[2][0] . " Origin: " . $consoles[2][1] . " Units Sold: " . $consoles[2][2];
 
+$something = array(
+    array(1, 2, 3),
+    array(1, 2, 3),
+    array(1, 2, 3)
+);
+
+for ($i=0; $i < 4; $i++) { 
+    for ($j=0; $j < 4; $j++) { 
+        echo "Array $i Element $j <br>";
+    };
+};
+*/
+
+$grade = array(
+    "Math" => 5,
+    "History" => 5,
+    "English" => 5
+);
+
+echo "Math grade is " . $grade["Math"];
+echo "<br>";
+foreach($grade as $subject => $grade) {
+    echo "Subject: " . $subject, ", Grade: " . $grade;
+    echo "<br>";      
+}
 
 ?>
