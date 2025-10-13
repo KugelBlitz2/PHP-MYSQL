03e2e8d94fc2d8358a34c6e11838f9a7e7f82b08
KugelBlitz2 <galica.orik@gmail.com>
2025-10-13 20:45:47 +0200

    1

---
+++ moduli3/app.php
@@ -0,0 +1,23 @@
+<?php
+
+//$file = fopen("file.csv", "w");
+
+
+$test = "fille.txt";
+
+$test2 = fopen($test, "r");
+
+$test3 = filesize($test);
+
+$test4 = fread($test2, $test3);
+
+echo $test4;
+
+$brug = fopen("fille.txt", "r");
+
+while (!feof($brug)) {
+    echo fgets($brug). "<br>";
+}
+
+fclose($brug);
+?>
