e2e4b3a78489b46fab8578bd0224c1ef63aca31f
KugelBlitz2 <galica.orik@gmail.com>
2025-10-22 20:45:54 +0200

    1

---
+++ moduli5/app.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="app.php" method="get">
+        <label for="Username">Username</label><br>
+        <br>
+        <input type="text" name="username" placeholder="test"><br>
+        <br>
+        <label for="Password">Password</label><br>
+        <br>
+        <input type="text" name="password" placeholder="test"><br>
+        <br>
+        <button>Button</button>
+    </form>
+</body>
+</html>
+
+<?php 
+$username = $_GET["username"];
+$password = $_GET["password"];
+
+echo "$username and $password"
+
+?>
