2b440b7392dbb6b99acf75cebfaf4bbbdad47c14
KugelBlitz2 <galica.orik@gmail.com>
2025-11-03 20:43:19 +0100

    1

---
+++ moduli6/test1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="test1.php" method="POST">
+    <input type="text" name="name" placeholder="name">
+    <br>
+    <input type="text" name="surname" placeholder="surname">
+    <br>
+    <input type="text" name="email" placeholder="email">
+    <br>
+    <button type="submit" name="submit">Submit</button>
+
+    </form>
+
+</body>
+</html>
